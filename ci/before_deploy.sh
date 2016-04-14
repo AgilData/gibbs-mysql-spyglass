fd637b7ee2488e9102944c32b1c4273e447fde0b
Brent Gardner <bgardner@squarelabs.net>
2016-04-14 14:52:49 -0600

    Deploy release artifacts

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ mk_tarball() {
 
   # update this part to copy the artifacts that make sense for your project
   # NOTE All Cargo build artifacts will be under the 'target/$TARGET/{debug,release}'
-  cp target/$TARGET/debug/spyglass $temp_dir
+  cp target/$TARGET/release/spyglass $temp_dir
 
   pushd $temp_dir
 
