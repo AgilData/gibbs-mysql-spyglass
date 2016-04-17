800060e85cae043279a20aa8a5f6afe18da317fd
Brent Gardner <bgardner@squarelabs.net>
2016-04-17 17:10:29 -0600

    More generalized distro checking

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ VERSION="v0.1.5"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         PLATFORM="x86_64-unknown-linux-gnu"
-        DISTRO=$(cat /etc/*-release | grep '^NAME=' | awk -F= '{print $2}' | sed 's/\"//g')
-        if [[ "$DISTRO" == "Amazon Linux AMI" ]]; then
+        DISTRO=$(cat /etc/*-release | grep '^ID_LIKE=' | awk -F= '{print $2}' | sed 's/\"//g')
+        if [[ "$DISTRO" == "fedora" ]]; then
                 yum install -y openssl-devel
-        elif [[ "$DISTRO" == "Ubuntu" ]]; then
+        elif [[ "$DISTRO" == "debian" ]]; then
                 apt-get install -y libssl-dev
         fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
