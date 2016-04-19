9bf9e575b0e16878f3b658be386b6a945455c747
Brent Gardner <bgardner@squarelabs.net>
2016-04-19 09:52:42 -0600

    Update for next release

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 BASE_URL="https://github.com/AgilData/gibbs-mysql-spyglass/releases/download"
-VERSION="v0.6.19"
+VERSION="v0.6.22"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         PLATFORM="x86_64-unknown-linux-gnu"
