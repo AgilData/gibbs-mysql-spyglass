9cb0f699842b6198ef03018830db3cc4f2d8c690
Brent Gardner <bgardner@squarelabs.net>
2016-04-18 22:36:18 -0600

    Fix for distrobutions without ID_LIKE

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 BASE_URL="https://github.com/AgilData/gibbs-mysql-spyglass/releases/download"
-VERSION="v0.6.18"
+VERSION="v0.6.19"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         PLATFORM="x86_64-unknown-linux-gnu"
         DISTRO=$(cat /etc/*-release | grep '^ID_LIKE=' | awk -F= '{print $2}' | sed 's/\"//g')
+        if [ -z "$DISTRO" ]; then
+			DISTRO=$(cat /etc/*-release | grep '^ID=' | awk -F= '{print $2}' | sed 's/\"//g')
+		fi  
+		echo DISTRO=$DISTRO
         if [[ "$DISTRO" == "fedora" ]]; then
                 yum install -y openssl-devel
                 PLATFORM="fedora"
